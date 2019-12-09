33e78454ecf243ad37064bc3a63cd8a96ced581f
Marci <tmarcikka@gmail.com>
2019-12-10 00:18:17 +0100

    network notebook minor reworks
    Use of new .py files
    custom_network.py corrected (untested)

--- network_utils/custom_network.py
+++ network_utils/custom_network.py
@@ -1,47 +1,50 @@
 import keras
 
-kernel_size = (6,8)
-filters = 64
+def CustomNetwork1():
+    kernel_size = (6,8)
+    filters = 64
 
-model = keras.models.Sequential()
-model.add(keras.layers.InputLayer(input_shape=(120, 160, 3)))    # RGB image goes in
+    model = keras.models.Sequential()
+    model.add(keras.layers.InputLayer(input_shape=(120, 160, 3)))    # RGB image goes in
 
-# ENCODER
-# --------------------
-for _ in range(3):
-    for _ in range(2):
-        model.add(keras.layers.Conv2D(filters=filters,
-                                        kernel_size=kernel_size,
-                                        strides=1,
-                                        padding='same',
-                                        use_bias=True,
-                                        bias_initializer=keras.initializers.constant(value=0.0)))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.ReLU())
+    # ENCODER
+    # --------------------
+    for _ in range(3):
+        for _ in range(2):
+            model.add(keras.layers.Conv2D(filters=filters,
+                                            kernel_size=kernel_size,
+                                            strides=1,
+                                            padding='same',
+                                            use_bias=True,
+                                            bias_initializer=keras.initializers.constant(value=0.0)))
+            model.add(keras.layers.BatchNormalization())
+            model.add(keras.layers.ReLU())
 
-    model.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
-    filters *= 2
-# --------------------
+        model.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
+        filters *= 2
+    # --------------------
 
-# DECODER
-# --------------------
-for _ in range(3):
-    filters = round(filters/2)
-    model.add(keras.layers.UpSampling2D(size=2,
-                                        interpolation='nearest'))
-    for _ in range(2):
-        model.add(keras.layers.Conv2D(filters=filters,
-                                        kernel_size=kernel_size,
-                                        strides=1,
-                                        padding='same',
-                                        use_bias=True,
-                                        bias_initializer=keras.initializers.constant(value=0.0)))
-        model.add(keras.layers.BatchNormalization())       
-        model.add(keras.layers.ReLU())
-# --------------------
+    # DECODER
+    # --------------------
+    for _ in range(3):
+        filters = round(filters/2)
+        model.add(keras.layers.UpSampling2D(size=2,
+                                            interpolation='nearest'))
+        for _ in range(2):
+            model.add(keras.layers.Conv2D(filters=filters,
+                                            kernel_size=kernel_size,
+                                            strides=1,
+                                            padding='same',
+                                            use_bias=True,
+                                            bias_initializer=keras.initializers.constant(value=0.0)))
+            model.add(keras.layers.BatchNormalization())       
+            model.add(keras.layers.ReLU())
+    # --------------------
 
-model.add(keras.layers.Dense(units=1,
-                                activation='sigmoid',
-                                use_bias=True
-))
-model.add(keras.layers.Reshape((120,160)))
+    model.add(keras.layers.Dense(units=1,
+                                    activation='sigmoid',
+                                    use_bias=True
+    ))
+    model.add(keras.layers.Reshape((120,160)))
+
+    return model
